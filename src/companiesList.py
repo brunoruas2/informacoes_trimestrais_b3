b86381b8fd7375d3406b2850b27ec3776323d17c
Bruno Ruas <bruno.ruas2@gmail.com>
2022-07-24 17:27:31 -0400

    companiesList

--- src/companiesList.py
+++ src/companiesList.py
@@ -9,7 +9,7 @@ PATH = 'src\zip'
 files = os.listdir('src\zip')
 
 companiesList = pd.DataFrame()
-for files in file:
+for file in files:
     zf = zip.ZipFile(PATH + '\\' + file)
     nameFile = zf.namelist()[0]
     companiesYear = pd.read_csv(zf.open(nameFile),encoding='ISO-8859-1',sep=';')
