67dfec3214fe234c408afaeb9e4efed8eb704f9b
Bruno Ruas <bruno.ruas2@gmail.com>
2022-07-24 17:19:58 -0400

    zip files download

---
+++ src/companiesList.py
@@ -0,0 +1,19 @@
+from base64 import encode
+import os
+import zipfile as zip
+import pandas as pd
+from pynvim import encoding
+
+PATH = 'src\zip'
+
+files = os.listdir('src\zip')
+
+companiesList = pd.DataFrame()
+for files in file:
+    zf = zip.ZipFile(PATH + '\\' + file)
+    nameFile = zf.namelist()[0]
+    companiesYear = pd.read_csv(zf.open(nameFile),encoding='ISO-8859-1',sep=';')
+    companiesYear = companiesYear[['CNPJ_CIA','DENOM_CIA','CD_CVM']]
+    companiesList = companiesList.append(companiesYear)
+
+companiesList = companiesList.drop_duplicates
---
+++ src/updateZipFiles.py
@@ -0,0 +1,32 @@
+import sys
+import requests
+import urllib.request as urlRequest
+
+# progress bar
+# https://twitter.com/clcoding/status/1549819582390894592
+
+def progressBar(count, total, suffix=''):
+    barLenght = 60
+    filledLength = int(round(barLenght * count / float(total)))
+    percent = round(100.0 * count / float(total), 1)
+    bar = '=' * filledLength + '-' * (barLenght - filledLength)
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percent, '%', suffix))
+    sys.stdout.flush()
+
+# getting the zip files
+url = requests.get('https://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/ITR/DADOS/').text
+url = url.split('.zip\">')
+
+files = []
+for item in url:
+    if item[0:3] == 'itr':
+        files.append(item.split('</a>')[0])
+
+# downloading the files into respository src\zip
+print('Downloading Files\n')
+
+for url in files:
+    progressBar(files.index(url)*2,len(url))
+    urlRequest.urlretrieve('https://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/ITR/DADOS/{}'.format(url),'src\zip\{}'.format(url))
+
+print('\n\nFiles Updated')
